0506c72b828b157a09c7c9b5d033ed0f024e8b7b
Honing <honing.d@163.com>
2016-07-15 17:00:10 +0800

    code-utf8
    Solve the coding-impatibility

--- JS实现人机大战-五子棋/test.php
+++ JS实现人机大战-五子棋/test.php
@@ -1,6 +1,7 @@
 <?php
+    header("Content-type:text/html;charset=utf-8");
 	$con = mysql_connect("localhost","root","");
-	$username=$_REQUEST['username'];
+	$username=urldecode($_REQUEST['username']);
 	$password=$_REQUEST['password'];
 	$times=$_REQUEST['times'];
 
@@ -8,8 +9,10 @@
   {
   die('Could not connect to mySQL: ' . mysql_error());
   }
-
+	 
 	mysql_select_db("chesslogindb", $con);
+	mysql_query("set character set 'utf8'");//读库 
+	mysql_query("set names 'utf8'");//写库
 
 	$sql="INSERT INTO userinfo (username, password, times) VALUES ('".$username."','".$password."','".$times."')";
 	
@@ -17,7 +20,7 @@
   {
   	die('Error: ' . mysql_error());
   }
-	echo "SUCCESS!";
+	echo "提交成功！";
 
 	mysql_close($con);
 ?>
