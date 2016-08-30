8c4fd65cdd0eede9f1f90e882354bef326918ce7
Honing <honing.d@163.com>
2016-08-30 15:50:44 +0800

    Changed to the online Mysql database and some displaying bugs
    1. Changed to the online Mysql database
    2. Solved the problem when zooming out the window the page shrinks with
    it.

--- JS实现人机大战-五子棋/conn.php
+++ JS实现人机大战-五子棋/conn.php
@@ -1,11 +1,11 @@
 <?php 
 /******************************数据库连接*****************************/
-$conn = @mysql_connect("localhost","root","");
+$conn = @mysql_connect("hujuntao.gotoftp1.com","hujuntao","19870630");
 if (!$conn){
     die("连接数据库失败：" . mysql_error());
 	echo("连接失败");
 }
-mysql_select_db("chess_db", $conn);
+mysql_select_db("hujuntao", $conn);
 //字符转换，读库
 mysql_query("set character set 'gbk'");
 //写库
